848017be997a4f22dc9bbb618f1671bf2a554647
Flyness <pumulonamatama@gmail.com>
2024-06-06 19:44:09 -0400

    First Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Home from './Home';
+import MeetDev from './MeetDev';
+import Login from './Login';
+import Support from './Support';
+
+
+function App() {
+  return (
+   <div>
+    <Home />
+    <Login />
+    <MeetDev />
+    <Support />
+   </div>
+  );
+}
+
+export default App;
---
+++ src/MeetDev.js
@@ -0,0 +1,5 @@
+function MeetDev (){
+
+};
+
+export default MeetDev
